101662b72088d168163aa58b5a47444701d3df61
Marcus Guerrand <57366572+marcusguerranda@users.noreply.github.com>
2023-01-27 16:05:25 -0500

    Update PricingEngine.hpp

--- PricingEngine.hpp
+++ PricingEngine.hpp
@@ -1,3 +1,9 @@
+// PricingEngine.hpp
+//
+//Purpose: Base class for other pricing engines: BSExactPricingEngine, DividedDifferences, etc.
+//
+//Modification dates: 12/30/2022 - 1/25/2023
+
 // The design pattern was inspired by Mark Joshi's "C++ Design Patterns and Derivatives Pricing"
 // as well as D.Duffy's article "Monte Carlo Methods in Quantitative Finance Generic and Efficient MC Solver in C++"
 
@@ -10,11 +16,11 @@ class PricingEngine
 private:
     //N/A
 
-public:
-    PricingEngine();
-    PricingEngine(const PricingEngine& source);
-    virtual ~PricingEngine();
-    PricingEngine& operator = (const PricingEngine& source);
+public: 
+    PricingEngine();                            // Default constructor
+    PricingEngine(const PricingEngine& source); // Copy constructor
+    ~PricingEngine();                           // Destructor
+    PricingEngine& operator = (const PricingEngine& source);    //Assignment operator
 
 };
 

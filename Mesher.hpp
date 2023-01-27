07c99b54bfe27870394195476b1861a288514c8f
Marcus Guerrand <57366572+marcusguerranda@users.noreply.github.com>
2023-01-27 16:04:39 -0500

    Update Mesher.hpp

--- Mesher.hpp
+++ Mesher.hpp
@@ -1,5 +1,9 @@
 //Mesher.hpp
-
+//
+//Purpose: Creating a mesh from starting and ending mesh points, and defining mesh point size. There is also << operator overloading and PrintVector() function
+//         to print the output results of the matrices after having been stored into a vector. This function sets mesh points with those data points within the vector provided, and prints out
+//         the appropriate information.
+//Modification dates: 12/30/2022 - 1/25/2023
 
 #ifndef Mesher_hpp
 #define Mesher_hpp
@@ -9,27 +13,29 @@
 #include <iostream>
 #include <sstream>
 
-enum class Param_Type
+enum class Param_Type //Parameter variables, in order to create appropriate mesh points 
 {
     //S=underlying asset price/ K=strike price/R=interest rate/Sig=volatility/T=time to expiry/h=parameter for divided differences
-    S,K,R,Sig,T,h       
+    S,K,R,Sig,T, h       
 };
 
+//Create mesh points with starting and ending mesh points, and uniform mesh size
 inline std::vector<double> Mesh_Generate(const double& start_mesh, const double& end_mesh, const double& size_mesh) 
 {
     std::vector<double> mesh;
-    double tmp = start_mesh; 
+    double tmp = start_mesh; //Take starting point and set it as the beginning of the vector of points
 
-	while(tmp <= end_mesh)
+	while(tmp <= end_mesh)   //Iterating through increments of mesh points by storing each new mesh point into the vector, and thus iterating till it reaches the end mesh point
     {
         mesh.push_back(tmp);
-        tmp+= size_mesh;
+        tmp+= size_mesh;    //Resetting value of departing mesh point with previous mesh point attained, and incrementing with uniform mesh size
     }
 
     return mesh;
 }
    
 // Inline definition or there will be a linking error due to GCC compiler.
+// << operator overloading to print out the type of parameter at hand
 inline std::ostream& operator << (std::ostream& os, const Param_Type& source_type) {
     switch (source_type) {
         case Param_Type::S:
@@ -58,7 +64,7 @@ inline void Print_Vector(const std::vector<double>& source_mesh, const std::vect
 {
     if(source_mesh.size() == source_results.size()) //Verifying if both vectors are of the same size
     {
-        for(int i = 0; i < source_mesh.size(); i++)
+        for(int i = 0; i < source_mesh.size(); i++) //For-loop iterating through mesh points, and getting associated output results from the vector provided
         {
             std::cout << source_type << " at: " << source_mesh[i] <<" = " << source_results[i] << std::endl;
         }
